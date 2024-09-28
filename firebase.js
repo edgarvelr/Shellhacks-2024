dc96d7b2bae1e2ddb34fb109a0f138f6d2d05dc0
Zaynerrr <zaynlatchman@gmail.com>
2024-09-27 23:47:45 -0400

    Setup Development Environment

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAoaRDwRoZr3KJPyyleTtdPYy3n0NOM93k",
+  authDomain: "panther-pal.firebaseapp.com",
+  projectId: "panther-pal",
+  storageBucket: "panther-pal.appspot.com",
+  messagingSenderId: "1023110627536",
+  appId: "1:1023110627536:web:b1210f5e44f3878a98ded5",
+  measurementId: "G-CBS3J0YZ2D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
